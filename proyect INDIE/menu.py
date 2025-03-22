cd1a6b65e2bf9c798830f0233c1009f1962a32a7
Anthony Manuel Sigarán <146805872+MN03SGO@users.noreply.github.com>
2025-03-21 20:32:37 -0600

    COMENTARIOS
    Lineas comentadas para mayor entendimiento

--- proyect INDIE/menu.py
+++ proyect INDIE/menu.py
@@ -5,7 +5,13 @@ from caracteres import Caracteres
 from mundo import Mundo
 
 pygame.init()
-WIDTH, HEIGHT = 800, 600
+
+info = pygame.display.Info()
+WIDTH, HEIGHT = 800, 600 #PANTALLA EN RECUADRO DE 800 x 600
+
+
+#WIDTH, HEIGHT = info.current_w, info.current_h  # (1-Pantalla completa)
+#ventana = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN) # (1-Pantalla completa, para acitvar solo descomenta esta linea y comenta la  otra de 800 x 600)
 
 ventana = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("DEMO JUEGO DS")
@@ -21,6 +27,16 @@ def main():
                 pygame.quit()
                 sys.exit()
 
+
+
+
+
+            # (salir del modo pantalla completa con la tecla ESC)
+            #if event.type == pygame.KEYDOWN:
+                #if event.key == pygame.K_ESCAPE:
+                    #pygame.quit()
+                    #sys.exit()
+
         ventana.fill(constants.WHITE)
         mundo.draw(ventana)
         caracteres.draw(ventana)
@@ -29,3 +45,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# LINEAS COMENTADAS PARA PANTALLA COMPLETA
+# -- Si queres trabajar en modo pantalla completa y probar los designe en mejor vision descomenta las lineas y comentas las lineas relacionadas a 800 x 600
+# --Tambien el evento de salir en pantalla completa con ESC
+# CUALQUIER, INTEGRACION O OTRA POR EL ESTILO ME AVISAS.
+# by sigaran
